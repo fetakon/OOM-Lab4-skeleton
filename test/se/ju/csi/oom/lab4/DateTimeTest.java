c1fc6d3ec958e066d7845da60cb3c70753c0123c
kaan17ak <kaan17ak@student.ju.se>
2018-10-06 16:39:47 +0200

    added dateTimeTest

---
+++ test/se/ju/csi/oom/lab4/DateTimeTest.java
@@ -0,0 +1,26 @@
+package se.ju.csi.oom.lab4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DateTimeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testToString() {
+		DateTime _dateTime = new DateTime(2018,10,6,16,37,0);
+		assertEquals("2018-10-06 16:37:00", _dateTime.toString());
+	}
+
+	@Test
+	public void testDateTimeString() {
+		DateTime _dateTime = new DateTime("2018-10-06 16:37:00");
+		assertEquals("2018-10-06 16:37:00", _dateTime.toString());
+	}
+
+}
